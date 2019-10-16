d52c3ba91ad9eec862c860f09e7d08193af13139
Arber Avdullahu <arber_919@live.com>
2019-10-16 15:28:41 +0200

    fixed the ordering

--- shortest_path/dag/dag.go
+++ shortest_path/dag/dag.go
@@ -6,19 +6,19 @@ import (
 	"github.com/arberiii/Graph-Algorithms/graph"
 )
 
-// it is assumed that we have a topological order, graph is reversed! and we have weight
+// it is assumed that we have a topological order, graph is adjacency list with N^-! and we have weight
 func DAG(g, w graph.Graph, order []int) graph.Graph {
-	d := make([]float64, len(order))
-	pi := make([]int, len(order))
+	d := make(map[int]float64)
+	pi := make(map[int]int)
 	for i := 0; i < len(order); i++ {
 		d[order[i]] = math.Inf(1)
 		pi[order[i]] = 0
 	}
 	d[order[0]] = 0
 	for i := 1; i < len(order)+1; i++ {
-		for c, j := range g[i] {
-			if d[order[j]]+float64(w[i][c]) < d[order[i]] {
-				d[order[i]] = d[order[i]] + float64(w[i][c])
+		for c, j := range g[order[i]] {
+			if d[j]+float64(w[order[i]][c]) < d[order[i]] {
+				d[order[i]] = d[j] + float64(w[order[i]][c])
 				pi[order[i]] = j
 			}
 		}
